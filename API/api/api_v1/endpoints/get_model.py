194b935185e2c2e8c18b8219d60c2b0c95051cc1
EasonC13 <pricean01@gmail.com>
2021-08-15 07:33:27 +0800

    feat: API development environment

---
+++ API/api/api_v1/endpoints/get_model.py
@@ -0,0 +1,42 @@
+from fastapi import APIRouter, Depends, status
+
+from typing import Optional
+
+from pydantic import BaseModel
+
+from typing import Any, Dict, AnyStr, List, Union
+
+JSONObject = Dict[AnyStr, Any]
+JSONArray = List[Any]
+JSONStructure = Union[JSONArray, JSONObject]
+
+router = APIRouter()
+
+
+@router.get("/model/label", tags = ["Status"], status_code=status.HTTP_200_OK)
+def get_model_status():
+    return {
+        "message": "get success",
+        "status": "re-training",
+        "step": {
+            "current": 30,
+            "total": 50,
+        },
+        "train-data-amount": 2500,
+        "affect-labels": ['Party', 'Race', 'SpecialTerm', 'TemporalUnit', 'Time', 'Timezone', 'US_States']
+    }
+
+
+
+@router.get("/model/template", tags = ["Status"], status_code=status.HTTP_200_OK)
+def get_model_status():
+    return {
+        "message": "get success",
+        "status": "re-training",
+        "step": {
+            "current": 30,
+            "total": 50,
+        },
+        "train-data-amount": 2500,
+        "affect-labels": ['Party', 'Race', 'SpecialTerm', 'TemporalUnit', 'Time', 'Timezone', 'US_States']
+    }
